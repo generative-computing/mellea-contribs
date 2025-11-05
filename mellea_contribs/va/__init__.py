2ecb726da74dde347e15e3a37aa3f9e2d776da61
Masataro Asai <guicho2.71828@gmail.com>
2025-11-05 16:12:11 -0500

    kwargs

--- mellea_contribs/va/__init__.py
+++ mellea_contribs/va/__init__.py
@@ -11,46 +11,46 @@ class YesNo(BaseModel):
 
 class Core:
 
-    def binary(m:MelleaSession, prompt:str):
+    def binary(m:MelleaSession, prompt:str, **kwargs):
 
         output = m.instruct(f"{prompt} Answer yes or no.",
-                            format=YesNo)
+                            format=YesNo, **kwargs)
 
         yesno = YesNo.model_validate_json(output.value)
 
         return yesno.answer == "yes"
 
 
-    async def abinary(m:MelleaSession, prompt:str):
+    async def abinary(m:MelleaSession, prompt:str, **kwargs):
 
         output = await m.ainstruct(f"{prompt} Answer yes or no.",
-                                   format=YesNo)
+                                   format=YesNo, **kwargs)
 
         yesno = YesNo.model_validate_json(output.value)
 
         return yesno.answer == "yes"
 
 
-    def choice(self:MelleaSession, prompt:str, choices:list[str]):
+    def choice(self:MelleaSession, prompt:str, choices:list[str], **kwargs):
 
         class Choice(BaseModel):
             answer : Literal[choices]
 
         output = self.instruct(f"{prompt} Respond with one of the following answers: " + ",".join([f"'{c}'" for c in choices]) + ".",
-                               format=Choice)
+                               format=Choice, **kwargs)
 
         choice = Choice.model_validate_json(output.value)
 
         return choice.answer
 
 
-    async def achoice(self:MelleaSession, prompt:str, choices:list[str]):
+    async def achoice(self:MelleaSession, prompt:str, choices:list[str], **kwargs):
 
         class Choice(BaseModel):
             answer : Literal[choices]
 
         output = await self.ainstruct(f"{prompt} Respond with one of the following answers: " + ",".join([f"'{c}'" for c in choices]) + ".",
-                                      format=Choice)
+                                      format=Choice, **kwargs)
 
         choice = Choice.model_validate_json(output.value)
 
