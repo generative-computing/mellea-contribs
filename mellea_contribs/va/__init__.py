48fa4211af5c092fa10a5a2ff6edec452a79f70b
Masataro Asai <guicho2.71828@gmail.com>
2025-11-05 15:59:22 -0500

    VA init

---
+++ mellea_contribs/va/__init__.py
@@ -0,0 +1,38 @@
+
+
+from mellea import MelleaSession
+
+from pydantic import BaseModel
+
+from typing import Literal
+
+class YesNo(BaseModel):
+    answer : Literal["yes","no"]
+
+class Core:
+
+    def binary(m:MelleaSession, prompt):
+
+        output = m.instruct(f"{prompt} Answer yes or no.",
+                            format=YesNo)
+
+        yesno = YesNo.model_validate_json(output.value)
+
+        return yesno.answer == "yes"
+
+
+    def choice(self:MelleaSession, prompt, choices:list[str]):
+
+        class Choice(BaseModel):
+            answer : Literal[choices]
+
+        output = self.instruct(f"{prompt} Respond with one of the following answers: " + ",".join([f"'{c}'" for c in choices]) + ".",
+                               format=Choice)
+
+        choice = Choice.model_validate_json(output.value)
+
+        return choice.answer
+
+
+
+
