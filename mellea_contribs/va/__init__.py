bfbb491e45067f2a7f9e74e58b90bdaacaaad29a
Masataro Asai <guicho2.71828@gmail.com>
2025-11-12 15:17:18 -0500

    renamed Submodular -> Subset

--- mellea_contribs/va/__init__.py
+++ mellea_contribs/va/__init__.py
@@ -695,22 +695,53 @@ Cluster.cluster = sync_wrapper(Sort.acluster)
 
 
 
-class Submodular(Core):
-    async def agreedy_submodular_maximization(m:MelleaSession,
-                                              criteria: str,
-                                              elems:list[str],
-                                              k:int,
-                                              *,
-                                              vote:int=3,
-                                              positional:bool=True,
-                                              **kwargs):
+class Subset(Core):
+    async def asubset(m:MelleaSession,
+                      description:str,
+                      criteria: str,
+                      elems:list[str],
+                      k:int,
+                      *,
+                      vote:int=3,
+                      positional:bool=True,
+                      **kwargs):
+        """
+        Greedily select a k-elements subset from elems, maximizing the given criteria.
+
+        Args:
+            description: A decription of what the current and the output subset is meant to represent.
+            criteria: A decription of the desired property of the returned subset.
+            elems: The universe to select the subset from.
+            k: The number of elements to select from elems.
+            vote: When >=1, it samples multiple selections in each turn, and perform a majority voting.
+            positional: Shuffle the order to present the elements to the LLM in order to mitigate the positional bias.
+
+        The criteria is assumed to be contain a modular or submodular aspect.
+
+        Example 1:
+
+        description = "We are building a team of culturally diverse members."
+
+        criteria = "Maximize the cultural diversity among the members."
+
+        Example 2:
+
+        description = ("We need set of past legal cases that helps defending our case. "
+                       "In our case, the defandant has ..."
+                       "We want to see a variety of cases that are relevant to ours but"
+                       "are also different from each other.")
+
+        criteria = "Minimize the ovelap with the documents in the current set while staying relevant to our case."
+        """
 
         current = []
         remaining = elems.copy()
 
         for _ in range(k):
-            chosen = await m.achoice(f"{criteria}\n" +
-                                     "The current set:\n" +
+            chosen = await m.achoice(f"{description}\n"
+                                     "Choose the best element to add to the current set following the criteria:\n"
+                                     f"Criteria: {criteria}\n" +
+                                     "Current set:\n" +
                                      "\n".join(current) + "\n",
                                      remaining,
                                      vote=vote,
@@ -722,4 +753,4 @@ class Submodular(Core):
         return current
 
 
-Submodular.greedy_submodular_maximization = sync_wrapper(Submodular.agreedy_submodular_maximization)
+Subset.subset = sync_wrapper(Subset.asubset)
