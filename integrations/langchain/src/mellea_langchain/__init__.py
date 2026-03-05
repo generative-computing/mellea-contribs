96937bef9bd22021d8f2df5ec2d5d4f8b261317e
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2026-03-05 11:29:17 -0500

    adding missing changes
    Signed-off-by: Akihiko Kuroda <akihikokuroda2020@gmail.com>

--- integrations/langchain/src/mellea_langchain/__init__.py
+++ integrations/langchain/src/mellea_langchain/__init__.py
@@ -3,9 +3,19 @@
 This package provides a LangChain-compatible chat model that wraps Mellea,
 enabling LangChain applications to use Mellea's generative programming
 capabilities through the standard LangChain interface.
+<<<<<<< HEAD
 """
 
 from .chat_model import MelleaChatModel
+=======
+
+It also provides output parsers and guardrails for validating LLM outputs
+using Mellea's requirements system.
+"""
+
+from .chat_model import MelleaChatModel
+from .guardrails import MelleaGuardrail, MelleaOutputParser, ValidationResult
+>>>>>>> aaec38e (adding missing changes)
 from .message_conversion import LangChainMessageConverter
 from .tool_conversion import LangChainToolConverter
 
@@ -13,6 +23,9 @@ __version__ = "0.1.0"
 
 __all__ = [
     "MelleaChatModel",
+    "MelleaGuardrail",
+    "MelleaOutputParser",
+    "ValidationResult",
     "LangChainMessageConverter",
     "LangChainToolConverter",
 ]
